fb5816fec74a98044572549d1e335fc4c76bc253
relliikz <100636896+relliikz@users.noreply.github.com>
2022-09-27 14:10:05 +1000

    3 files modified

--- models/spin.ts
+++ models/spin.ts
@@ -1,5 +1,5 @@
-import { BodyParts } from "./bodyParts.enum";
-import { Colours } from "./colours.enum";
+import { BodyParts } from './bodyParts.enum';
+import { Colours } from './colours.enum';
 
 export interface ISpin {
   colour: Colours;
@@ -8,6 +8,10 @@ export interface ISpin {
 
 // TODO: create a SpinRecord class which implements ISpin and adds a new attribute num:number
 export class SpinRecord implements ISpin {
-
-
+  num: number;
+  constructor(public colour: Colours, public bodyPart: BodyParts) {
+    this.num = 0;
+    this.colour = colour;
+    this.bodyPart = bodyPart;
+  }
 }
